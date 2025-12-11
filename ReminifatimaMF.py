af91849ccd9d220e98b81d6f78de61b6768423a3
reminifatimaMF <reminifatima8@gmail.com>
2025-12-11 22:20:54 +0100

    Update ReminifatimaMF.py

--- ReminifatimaMF.py
+++ ReminifatimaMF.py
@@ -15,7 +15,7 @@ data={
 df = pd.DataFrame(data)
 
 # *** Première partie: Création et affichage ***
-print("Analyse des Séquence ADN")
+print("Analyse des Séquences ADN")
 print(df)
 print("\n")
 
@@ -33,15 +33,15 @@ print("************* Affichage de la colonne Longueur *************\n")
 col_longueur = df["Longueur"]
 print(col_longueur)
 
-#3) filtrer les séquences avec la Longueur est supérieur a 10
-print("*********** Filtrage Avec pourcentage % ***********") 
+#3) filtrer les séquences avec la Longueur est supérieur à 10
+print("*********** Filtrage: Longueur supérieurà 10 ***********") 
 #Filtrer les séquences avec la Longueur est supérieur à 10
 filtered_df=df[df["Longueur"]>10]
 print(filtered_df)
 
-#4) calculer la moyenne du pourcentage de GC aprés la virgule 
+#4) calculer la moyenne du pourcentage de GC après la virgule 
 print("*********** Calcul de la moyenne ***********")
-#Calculer la moyenne du pourcentage de GC Avec 3 chiffre aprés la virgule
+#Calculer la moyenne du pourcentage de GC Avec 3 chiffres après la virgule
 average_gc = df["Pourcentage GC"].mean()
 print(f"Pourcentage moyen de GC:{average_gc:.3f}%")
 
@@ -50,6 +50,6 @@ print("************* Ajouter une nouvelle colonne catégorie GC *************")
 #Ajouter une nouvelle colonne "catégorie GC"
 df["Catégorie GC"] = df["Pourcentage GC"].apply(lambda x: "Riche" if x > 55 else ("Moyen" if 45 <= x <= 55 else "Faible"))
 print(df)
-#6) Ajouter colone du nombre de 'G'
+#6) Ajouter colonne du nombre de 'G'
 df["Nb_G"] = df["Séquence"].apply(lambda seq:seq.count("G"))
 print(df)
