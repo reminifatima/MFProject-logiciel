77a8a9f451a58101b0cde4157104880da9d80732
reminifatimaMF <reminifatima8@gmail.com>
2025-12-11 22:11:31 +0100

    Update ReminifatimaMF.py

--- ReminifatimaMF.py
+++ ReminifatimaMF.py
@@ -7,20 +7,20 @@ import pandas as pd
 
 #Données: Séquences ADN, Longueur, Pourcentage de GC
 data={
-  "Séquence": ["ATGCGTACGTA","GCTAGCTAGGCC","GCTAGCTAGGCC","TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGT"],
+  "Séquence": ["ATGCGTACGTA","GCTAGCTAGGCC","GCTAGCTAGGCC","TACGATCGTA","ATGAAAGGCTT","CGTACGTAGC","TTAACCGGAT"],
   "Longueur": [12,12,12,10,11,10,10],
-  "Pourcentage GC": [50,66.67,58.33,40.45,45,60,50]
+  "Pourcentage GC": [50,66.67,58.33,40,45.45,60,50]
   }
 #Création du DataFrame (tableau pandas)
 df = pd.DataFrame(data)
 
-# *** Prmière partie: Création et affichage ***
+# *** Première partie: Création et affichage ***
 print("Analyse des Séquence ADN")
 print(df)
 print("\n")
 
-# *** Deuxième partie: Opréations ***
-print("******* Opréations *******")
+# *** Deuxième partie: Opérations ***
+print("******* Opérations *******")
 
 # 1) Sélectionner la colonne "Séquence"
 sequences = df["Séquence"]
@@ -33,7 +33,7 @@ print("************* Affichage de la colonne Longueur *************\n")
 col_longueur = df["Longueur"]
 print(col_longueur)
 
-#3) filtrer les séquences avec la Longueur est supérieurr a 10%
+#3) filtrer les séquences avec la Longueur est supérieur a 10
 print("*********** Filtrage Avec pourcentage % ***********") 
 #Filtrer les séquences avec la Longueur est supérieur à 10
 filtered_df=df[df["Longueur"]>10]
@@ -41,7 +41,7 @@ print(filtered_df)
 
 #4) calculer la moyenne du pourcentage de GC aprés la virgule 
 print("*********** Calcul de la moyenne ***********")
-#Calculer la moyenne du pourcentage de GC Avec 3 chifrre aprés la virgule
+#Calculer la moyenne du pourcentage de GC Avec 3 chiffre aprés la virgule
 average_gc = df["Pourcentage GC"].mean()
 print(f"Pourcentage moyen de GC:{average_gc:.3f}%")
 
